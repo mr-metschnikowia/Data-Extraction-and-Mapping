cd1d05f67ce4b9dfcf881de17906cf89d3c08e17
mr-metschnikowia <67706430+mr-metschnikowia@users.noreply.github.com>
2020-07-30 13:06:51 +0100

    Update Data_Extraction_and_Mapping.py

--- Data_Extraction_and_Mapping.py
+++ Data_Extraction_and_Mapping.py
@@ -2,6 +2,8 @@ import os
 
 print(os.getcwd())
 
+# Provides working directory.
+
 reference = input('Name of File containing reference sequence (DNA/Protein) in FASTA format:')
 
 with open(reference, 'r') as f:
@@ -10,6 +12,13 @@ with open(reference, 'r') as f:
 file_data = "".join([i for i in file_data if i.isalpha()])
 
 file_data = file_data.upper()
+
+# Reads fasta file containing reference sequence and converts it into a continuous sting with no special characters, all upper case.
+
+bearing_4 = file_data.find('SEQUENCE') + len('SEQUENCE')
+file_data = file_data[bearing_4: len(file_data)]
+
+# Cuts first line off (containing strain info etc).
 
 SOI = input('Name of file containing sequence of gene of interest include file extension (.gff/.gb/.fasta):')
 start = len(SOI) - 2
@@ -21,10 +30,22 @@ if file_type == 'gb':
 if file_type == 'ff':
     switch = 2
 
+# Takes name of file containing protein/DNA sequence of gene (.fasta/.gb/.gff).
+# Determines file extension based on last two characters of file name. Value of switch is changed accordingly to allow for relevant extraction operation.
+
 with open(SOI,'r') as z:
     target = z.read()
 
 target = "".join([i for i in target if i.isalnum()])
+
+# File containing protein/DNA sequence of gene is read and converted into continuous string with all special characters and numbers removed.
+
+if file_type == 'ta':
+    bearing_5 = target.find('sequence') + len('sequence')
+    target = target[bearing_5: len(target)]
+    print(target[0:1000])
+
+# If file extension is .fasta, first line is cut off.
 
 if switch == 1:
     locus_tag = input('Please paste locus tag here:')
@@ -58,11 +79,25 @@ elif switch == 2:
         print(target)
         switch = 0
 
+# Operations for data extraction from .gb and .gff files, respectively. Locus tag/ gene id is used to navigate to relevant region.
+# All of the string prior to that is essentially chopped off. Relevant code word e.g. 'proteinsequence' is then used to navigate to
+# relevant region. Everything after that code word until a second code word e.g. 'end gene' is extracted. 
+
 if switch == 0:
+    target = target.upper()
     for i in range(len(file_data)-(len(target) - 1)):
         x = file_data[i]
         if file_data[i:i + len(target)] == target:
-            match = file_data[i - 500:i + len(target) + 500]
+            if i >= 499:
+                h = 500
+            else:
+                h = i
+            match = file_data[i - h:i + len(target) + 500]
+            print(i)
+            print(len(match))
             print(match)
             print(len(match) - len(target))
             print('Done')
+
+# Mapping: A for loop iterates over the reference sequence using a window of equal length to the gene sequence.
+# Any matches are returned. 
